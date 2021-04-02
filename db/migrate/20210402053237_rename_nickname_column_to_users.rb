a4bc112e591461472dd416febf3a6362163c0b94
tomomi94 <kurochan1004@gmail.com>
2021-04-02 14:33:54 +0900

    userテーブル_カラム名変更

---
+++ db/migrate/20210402053237_rename_nickname_column_to_users.rb
@@ -0,0 +1,5 @@
+class RenameNicknameColumnToUsers < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :users, :nickname, :accountname
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_043323) do
+ActiveRecord::Schema.define(version: 2021_04_02_053237) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", null: false
+    t.string "accountname", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
