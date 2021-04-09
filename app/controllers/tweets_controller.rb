38919d45aa7ca3e6abe76ebe7fb1b0887aca7434
tomomi94 <kurochan1004@gmail.com>
2021-04-09 19:41:07 +0900

    ActiveStorage導入

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,11 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.order('created_at DESC')
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
